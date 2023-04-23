32b52cf5aa776c3362a9e6c5d9878eac18534aeb
tirupathiraobevara <tirupathiraobevara@gmail.com>
2023-04-23 09:35:46 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+def home(request):
+    return render(request,'app1/home.html')
+def profile(request):
+    return render(request,'app1/profile.html')
+
+# Create your views here.
